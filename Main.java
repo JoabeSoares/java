949351ab07a9872d2072f0e01929d74f53664850
Joabe Soares Teixeira de Souza <joabe.soares1@gmail.com>
2023-04-20 08:52:12 -0300

    java

--- Main.java
+++ Main.java
@@ -1,32 +1,14 @@
-import java.util.Locale;
-
 public class Main {
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
+        double a;
+        int b;
 
-        String product1 = "Computer";
-        String product2 = "Office Desk";
+        a = 5.0;
+        b = (int) a;
 
-        byte age = 30;
-        int code = 5290;
-        char gender = 'F';
+    
 
-        double price1 = 2100.00;
-        double price2 = 650.50;
-        double measure = 53.234567;
-
-        System.out.println("Products:");
-        System.out.printf("%s, which price is %.2f%n", product1, price1);
-        System.out.printf("%s, which price is %.2f%n", product2, price2);
-
-        System.out.printf("Record: %d years old, code %d and gender: %s%n", age, code, gender);
-
-        System.out.printf("Measure with eight decimal places: %.8f%n", measure);
-        System.out.printf("Rouded: %.3f%n", measure);
-        Locale.setDefault(Locale.US);
-        System.out.printf("US decimal point: %.3f%n", measure);
+        System.out.println(b);
     }
 }
 
