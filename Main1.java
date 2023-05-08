583fb92a1d5b8bc1e66077531e91780eb3c78f7b
Joabe Soares Teixeira de Souza <joabe.soares1@gmail.com>
2023-05-08 11:06:23 -0300

    java

--- Main1.java
+++ Main1.java
@@ -1,33 +1,30 @@
-import java.util.Locale;
+import java.util.Scanner;
 
-public class Main1 {
-
-public class exercicios {
-    /**
-     * @param args
-     */
+public class Main1{
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        String product1 = "Computer";
-        String product2 = "Office Desk";
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        while (x != 0 && y != 0){
+            if (x > 0 && y > 0) {
+                System.out.println("Primeiro");
+            }   
+            else if (x < 0 && y > 0){
+                System.out.println("Segundo");
+            }   
+            else if (x < 0 && y < 0){
+                System.out.println("Terceiro");
+            } 
+            else {
+                System.out.println("Quarto");
+            }
+            x = sc.nextInt();
+            y = sc.nextInt();
+        }
+        
 
-        byte age = 30;
-        int code = 5290;
-        char gender = 'F';
-
-        double price1 = 2100.00;
-        double price2 = 650.50;
-        double measure = 53.234567;
-
-        System.out.println("Products:");
-        System.out.printf("%s, which price is %f%n %s, which price is %f", product1, price1, product2, price2);
-
-        System.out.printf("Record: %d years old, code %d and gender: %s", age, code, gender);
-
-        System.out.printf("Measure with eight decimal places: %.8f", measure);
-        System.out.printf("Rouded: %.3f", measure);
-        Locale.setDefault(Locale.US);
-        System.out.printf("US decimal point: %.3f", measure);
+        sc.close();
+        }
     }
-}
-}
