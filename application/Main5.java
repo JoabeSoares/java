1673f88fa7e3ff54ed41e0f9f7a8f18cee0eebc1
Joabe Soares Teixeira de Souza <joabe.soares1@gmail.com>
2023-07-25 23:02:15 -0300

    java

--- application/Main5.java
+++ application/Main5.java
@@ -3,7 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+public class Main5 {
     public static void main(String[] args) {
         
         Locale.setDefault(Locale.US);
@@ -13,11 +13,11 @@ public class Program {
         System.out.println("Enter the measures of triangle X: ");
         xA = sc.nextDouble();
         xB = sc.nextDouble();
-        xB = sc.nextDouble();
+        xC = sc.nextDouble();
         System.out.println("Enter the measures of triangle Y: ");
         yA = sc.nextDouble();
         yB = sc.nextDouble();
-        yB = sc.nextDouble();
+        yC = sc.nextDouble();
 
         double p = (xA + xB + xC) / 2.0;
         double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
